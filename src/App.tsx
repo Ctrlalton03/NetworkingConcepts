6eb6269b7be78416708a609d3ffee1546f5cd9c6
Ctrlalton03 <altonsimpson22@gmail.com>
2024-09-22 20:56:54 -0400

    final

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Header from './Components/Header';
 import Home from './Components/Homepage';
 import NetworkingConcepts from './Components/NetworkingConcepts';
 import Reflection from './Components/Reflection';
-import AdditionalResources from './Components/AdditionalResources';
 import Contact from './Components/Contact';
 import footer from './Components/Footer';
 
@@ -16,7 +15,6 @@ const app: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/networking-concepts" element={<NetworkingConcepts />} />
         <Route path="/reflections" element={<Reflection />} />
-        <Route path="/additional-resources" element={<AdditionalResources />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <footer />
